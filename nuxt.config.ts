e53d49d444ef1a6a587e6c0c9133770c52a63c49
Kintsuba <bnxth107@gmail.com>
2025-09-26 21:26:24 +0900

    feat: use @nuxt/eslint instead of prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-09-25",
 
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+
   css: ["~/assets/css/main.css"],
+
+  eslint: {
+    config: {
+      stylistic: true, // <---
+    },
+  },
 });
